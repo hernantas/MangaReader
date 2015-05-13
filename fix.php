b3fd6697eaed0eba423cdc8f799e52c76bb0d678
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2015-05-13 21:09:41 +0700

    Fix domdomsoft bookmark creator

--- fix.php
+++ fix.php
@@ -3,16 +3,17 @@
 	$q = $db->Query("select manga_name.*, COUNT(manga_chapter.id) as ch_cnt from manga_name, manga_chapter where manga_name.id=manga_chapter.id_manga group by manga_name.id");
 	
 	$limit = -1;
-	echo "<textarea><Bookmarks>\n";
+	echo "<textarea cols=100 rows=30><Bookmarks>\n";
 	while ($data = mysql_fetch_array($q))
 	{
+		$name = htmlentities(htmlentities($data['name']));
 		if ($limit == 0) break;
 		echo "  <Bookmark>\n";
-		echo "    <MangaLink>http://mangafox.com/manga/";
+		echo "    <MangaLink>http://www.mangahere.co/manga/";
 		echo str_replace("&", "_", str_replace(")", "", str_replace("(", "", str_replace(" ", "_", strtolower($data['name'])))));
 		echo "/</MangaLink>\n";
 		echo "    <OldNumChapters>".$data['ch_cnt']."</OldNumChapters>\n";
-		echo "    <MangaName>".$data['name']."</MangaName>\n";
+		echo "    <MangaName>".$name."</MangaName>\n";
 		echo "  </Bookmark>\n";
 		$limit--;
 	}
