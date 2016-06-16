71b32bc88abee083ae57d5f7690949ab4d192ec3
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-06-16 22:13:17 +0700

    Add readme to index.php file

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+    /**
+     * PHP Code Enhancement Framework
+     *
+     * PHP Code Enhancement or PCE in short is small project (part of Code Enhancement
+     * project) created to help making web application using PHP easier and more
+     * efficient by writing less. PCE purpose is to provide class and method so
+     * developer will write less code, manage their code better and can share code
+     * with other easily. PCE will be modular as much as possible by splitting project
+     * or package into it own directory and can be integrated into website application
+     * easily.
+     *
+     * @copyright Hernantas 2016
+     * @license MIT
+     * @link http://www.hernantas.com
+     */
+
+?>
