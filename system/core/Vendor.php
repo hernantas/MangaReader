ad4c83453f23605c9d50bb8a53393d45562e5a59
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-07-12 16:29:20 +0700

    Add vendor class to automatically manage vendor

---
+++ system/core/Vendor.php
@@ -0,0 +1,31 @@
+<?php
+
+    /**
+     * Vendor Class
+     *
+     * Manage vendor and can automatically detect vendor.
+     *
+     * @package Core
+     */
+    class Vendor
+    {
+        public function __construct()
+        {
+            $dirs = scandir(BASE_PATH);
+            $length = count($dirs);
+            $arr = array();
+            for ($i=0;$i<count($dirs);$i++)
+            {
+                if ($dirs[$i] != '.' && $dirs[$i] != '..' && $dirs[$i] != '.git' &&
+                    is_dir($dirs[$i]))
+                {
+                    $arr[] = $dirs[$i];
+                }
+            }
+
+            $config =& loadClass('Config', 'Core');
+            $config->save('Vendor', $arr);
+        }
+    }
+
+?>
