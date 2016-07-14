881c6a8e46aa7ca8985e226eaa035b10a87b9fa6
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-07-14 10:37:48 +0700

    Add Router Class to handle url routing

---
+++ system/core/Router.php
@@ -0,0 +1,67 @@
+<?php
+
+    class Router
+    {
+        private $route = array();
+
+        public function __construct()
+        {
+            $config =& loadClass('Config', 'Core');
+            $cfg = $config->load('Routing');
+
+            if ($cfg !== false)
+            {
+                foreach ($cfg as $key => $value)
+                {
+                    $this->addRoute($key, $value);
+                }
+            }
+        }
+
+        /**
+         * Add route by using url rule
+         *
+         * @param string $rule    URL Rule
+         * @param string $handler URL Handler
+         */
+        public function addRoute($rule, $handler)
+        {
+            $rule = trim(rtrim($rule, '/'), '/');
+            $handler = trim(rtrim($handler, '/'), '/');
+            $this->route[$rule] = $handler;
+        }
+
+        /**
+         * Find route based on rule.
+         *
+         * @param  string $url URL to match the rule
+         *
+         * @return string      Route based on available rule if available
+         */
+        private function findRoute($url)
+        {
+            foreach ($this->route as $key=>$val)
+            {
+                $pos = strpos($url, $key);
+                if ($pos === 0 || $pos === 1)
+                {
+                    return $val;
+                }
+            }
+
+            return $url;
+        }
+
+        /**
+         * Start routing proccess
+         */
+        public function routing()
+        {
+            $uri =& loadClass('Uri', 'Core');
+            $url = $this->findRoute($uri->string());
+
+            echo $url;
+        }
+    }
+
+?>
