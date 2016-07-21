93725d758f4f573a4cf3f034a88dc69c4e38d35b
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-07-21 22:22:39 +0700

    Add method to get row from result

--- system/db/Result.php
+++ system/db/Result.php
@@ -5,6 +5,7 @@
     {
         private $sql;
         private $data;
+        private $dataLength = 0;
 
         private $pos = 0;
 
@@ -12,16 +13,87 @@
         {
             $this->sql = $sql;
             $this->data = $data;
+            $this->dataLength = count($data);
         }
 
+        /**
+         * Get syntax used when performing the query
+         *
+         * @return string SQL Syntax
+         */
         public function syntax()
         {
             return $this->sql;
         }
 
+        /**
+         * Change result pointer position
+         *
+         * @param  string $pos Pointer position
+         */
+        public function seek($pos)
+        {
+            $this->pos = ($pos > $this->dataLength) ? $this->dataLength-1: $pos;
+        }
+
+        /**
+         * Reset result pointer to first row element.
+         */
+        public function reset()
+        {
+            $this->pos = 0;
+        }
+
+        /**
+         * Get query result per row
+         *
+         * @return array Result row
+         */
         public function row()
         {
-            return isset($this->data[$this->pos]) ? $this->data[$this->pos++] : false;
+            return $this->at($this->pos++);
+        }
+
+        /**
+         * Get query result count
+         *
+         * @return int Query result count
+         */
+        public function count()
+        {
+            return $this->dataLength;
+        }
+
+        /**
+         * Get first row from query result data
+         *
+         * @return array First row on Query result
+         */
+        public function first()
+        {
+            return $this->at(0);
+        }
+
+        /**
+         * Get query result at specific row
+         *
+         * @param  int $pos Row number, start with 0
+         *
+         * @return array    Row on query result
+         */
+        public function at($pos)
+        {
+            return isset($this->data[$pos]) ? $this->data[$pos] : false;
+        }
+
+        /**
+         * Get last row from query result
+         *
+         * @return array Last Row on query result
+         */
+        public function last()
+        {
+            return $this->at($this->dataLength-1);
         }
     }
 
