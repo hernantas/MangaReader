782b0b78427706f717f2780b028962e185001afa
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-07-21 21:22:01 +0700

    Add doc to laoder method

--- system/core/Loader.php
+++ system/core/Loader.php
@@ -11,6 +11,13 @@
     {
         private $instance = array();
 
+        /**
+         * Load a page. This function shouldn't be called normally and only be used
+         * on routing class which will call specific handler page.
+         *
+         * @param  string $name   Page name
+         * @param  string $method Method in Page class that need to be called
+         */
         public function page($name, $method='index')
         {
             if ($name==='')
@@ -30,6 +37,20 @@
             }
         }
 
+        public function dbDriver($name)
+        {
+            return $this->loadClass($name, 'DB/Driver');
+        }
+
+        /**
+         * Find and load file if file is exists.
+         *
+         * @param  string $name    File name
+         * @param  string $package Package where file is placed
+         * @param  array  $data    Data that need to be passed to the file
+         *
+         * @return bool            True if file is exists and loaded, false otherwise
+         */
         private function loadFile($name, $package='library', $data=array())
         {
             $vendors =& loadClass('Vendor', 'Core');
@@ -47,6 +68,14 @@
             return false;
         }
 
+        /**
+         * Load class and it's file if file
+         *
+         * @param  string $name    Class name
+         * @param  string $package Package name where file class is located
+         *
+         * @return object          Instance of the class
+         */
         private function &loadClass($name, $package='library')
         {
             $vendors =& loadClass('Vendor', 'Core');
