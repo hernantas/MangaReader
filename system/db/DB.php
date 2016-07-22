1025392988162f10b2c7ab8382156aec5bef7c28
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-07-22 17:00:00 +0700

    Add doc to method on DB class

--- system/db/DB.php
+++ system/db/DB.php
@@ -1,7 +1,7 @@
 <?php
     namespace DB;
 
-    include (SYSTEM_PATH . 'db/IDriver.php');
+    include (SYSTEM_PATH . 'db/driver/IDriver.php');
 
     class DB
     {
@@ -20,22 +20,48 @@
             }
         }
 
+        /**
+         * Load driver based on its string name if it exists
+         *
+         * @param  string $name Driver name (ex: mysqli)
+         */
         public function selectDriver($name)
         {
             $loader =& loadClass('Loader', 'Core');
             $this->driver = $loader->dbDriver($name);
         }
 
+        /**
+         * Connect to database host
+         *
+         * @param  string $host     Database host name
+         * @param  string $user     Username for database host
+         * @param  string $password Password for database host
+         */
         public function connect($host, $user, $password='')
         {
             $this->driver->connect($host, $user, $password);
         }
 
+        /**
+         * Select database inside the database host
+         *
+         * @param  string $name   Database name
+         */
         public function database($name)
         {
             $this->driver->database($name);
         }
 
+        /**
+         * Perform query syntax. Support data binding and should be used if passing
+         * user inputed data to database.
+         *
+         * @param  string $sql  SQL syntax
+         * @param  array  $data Data that need to be binded to performed SQL
+         *
+         * @return \DB\Result     Database result
+         */
         public function query($sql, $data=[])
         {
             if (count($data) > 0)
