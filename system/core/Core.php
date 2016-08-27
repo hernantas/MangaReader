3f842f7c0553d5ca422a3e275cec9148af5bc02f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-27 18:57:21 +0700

    Bootstrap Input class

--- system/core/Core.php
+++ system/core/Core.php
@@ -3,6 +3,7 @@
     require (SYSTEM_PATH . 'core/Common.php');
 
     $config =& loadClass('Config', 'Core');
+    $input =& loadClass('Input', 'Core');
     $vendor =& loadClass('Vendor', 'Core');
     $uri =& loadClass('URI', 'Core');
     $router =& loadClass('Router', 'Core');
