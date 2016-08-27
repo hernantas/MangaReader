25ec508e3675b2d64e4debec9c526587af12b423
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-27 18:54:53 +0700

    Add doc to session class method

--- system/library/Session.php
+++ system/library/Session.php
@@ -17,12 +17,24 @@
          */
         private $longExpire = 31536000;
 
+        /**
+         * Set session
+         *
+         * @param string $name    Session name
+         * @param string $value   Session value
+         * @param bool   $persist Set if session is persistent (have long expire time), or not.
+         */
         public function set($name, $value, $persist=false)
         {
             setcookie($name, $value, time()+($persist?$this->longExpire:$this->expire),
                 page()->uri->subdir());
         }
 
+        /**
+         * Remove session
+         *
+         * @param  string $name Session name
+         */
         public function remove($name)
         {
             setcookie($name, '', -1, page()->uri->subdir());
