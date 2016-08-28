4c8f13956c6b9f9effbdcf5e6f6f462c031de3b2
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-28 21:10:13 +0700

    Change signup page to create an account and fix sign in link

--- app/view/SignUp.php
+++ app/view/SignUp.php
@@ -1,7 +1,7 @@
 <div class="single">
     <div class="panel warp">
         <?php echo formOpen('user/signup'); ?>
-        <h1>Sign Up</h1>
+        <h1>Create Account</h1>
         <div>
             <?php echo inputText('username', 'Username, 5-16 characters.'); ?>
         </div>
@@ -12,10 +12,10 @@
             <?php echo inputPassword('rpassword', 'Retype your password.'); ?>
         </div>
         <div>
-            <?php echo inputSubmit('Sign In'); ?>
+            <?php echo inputSubmit('Create my account'); ?>
         </div>
         <div>
-            Already have account? <a href="<?php echo baseUrl(); ?>user/signup">Sign In</a>
+            Already have account? <a href="<?php echo baseUrl(); ?>user/signin">Sign In</a>
         </div>
         <?php echo formClose(); ?>
     </div>
