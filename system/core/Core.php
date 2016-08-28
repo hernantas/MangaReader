e674a1cbc4bac24244a3d276742a3fe95ef9ba7d
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-29 00:13:05 +0700

    Bootstrap current load system

--- system/core/Core.php
+++ system/core/Core.php
@@ -3,13 +3,22 @@
     require (SYSTEM_PATH . 'core/Common.php');
 
     $config =& loadClass('Config', 'Core');
-    $input =& loadClass('Input', 'Core');
     $vendor =& loadClass('Vendor', 'Core');
+
+    $input =& loadClass('Input', 'Core');
     $uri =& loadClass('URI', 'Core');
+
+    $hook =& loadClass('Hook', 'Core');
+    $loader =& loadClass('Loader', 'Core');
     $router =& loadClass('Router', 'Core');
 
     $db =& loadClass('DB', 'db');
 
-    $router->routing();
-    
+    $page =& loadClass('Page', 'Core');
+    $curPage =& $page->getInstance();
+    $method = $router->method;
+
+    $loader->autoload();
+
+    $curPage->$method();
 ?>
