748765ffb3de9065f7b726c948d015f3f8452fff
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-28 21:11:11 +0700

    Sign Up now will show message if error and redirect if success

--- app/page/SignUp.php
+++ app/page/SignUp.php
@@ -5,9 +5,45 @@
     {
         public function index()
         {
+            if ($this->input->hasPost('username'))
+            {
+                $this->addUser();
+            }
+
             $this->load->storeView('SignUp');
 
             $this->load->layout('Fresh');
+        }
+
+        public function addUser()
+        {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $rpassword = $this->input->post('rpassword');
+
+            $msg = $this->load->library('Message');
+            $valid = $this->load->library('Validation');
+
+            if ($password !== $rpassword)
+            {
+                $msg->error('Your retype password is not match with the password you entered.');
+            }
+
+            if (!$valid->username($username))
+            {
+                $msg->error('Your username is not valid, only use alphabet (a-z), number (0-9),'.
+                    ' and symbol (_.-). The character length must between 5-16 characters');
+            }
+
+            if (!$valid->password($password))
+            {
+                $msg->error('Your password is not valid, only use alphabet (a-z), number (0-9),'.
+                    ' and symbol (@#!$*&~;:,?_.-). The character length must between 5-16 characters');
+            }
+
+            $msg->success('Your account has been created successfully.'.
+                ' Enter with your username and password', true);
+            $this->router->redirect('user/signin');
         }
     }
 
