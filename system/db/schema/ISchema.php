6b87f580ba84e322e2960841756d0a66a4efd202
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-29 15:42:28 +0700

    Schema now must have method hasTable and addConstraint

--- system/db/schema/ISchema.php
+++ system/db/schema/ISchema.php
@@ -4,6 +4,8 @@
     interface ISchema
     {
         public function addField($field);
+        public function addConstraint($cons);
+        public function hasTable($name);
         public function create($name);
     }
 
