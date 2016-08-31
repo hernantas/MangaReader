f6485b30da1255dcc0fd32dca35671bd58375878
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-08-31 13:03:13 +0700

    Now install library will redirect to the current install page if outside the installation page

--- system/library/Installation.php
+++ system/library/Installation.php
@@ -21,7 +21,8 @@
 
             if ($cfg !== false)
             {
-                $info = $config->loadInfo('Install');
+                $info = $this->config->loadInfo('Install');
+
                 if ($info !== false)
                 {
                     $this->progressOrder = $info;
@@ -42,7 +43,37 @@
 
         private function install()
         {
+            $state = array();
+            $notDone = array();
+            foreach ($this->installOrder as $order)
+            {
+                if (isset($this->progressOrder[$order]) &&
+                    $this->progressOrder[$order] == 'done')
+                {
+                    $state[$order] = 'done';
+                }
+                else
+                {
+                    $notDone[] = $order;
+                    $state[$order] = '';
+                }
+            }
 
+            // Resave config info incase progress order is empty
+            $this->config->saveInfo('Install', $state);
+
+            if (isset($notDone[0]))
+            {
+                $ex = explode('/', $notDone[0]);
+                $class = $ex[0]!==''?strtolower($ex[0]):'home';
+                $method = isset($ex[1])?strtolower($ex[1]):'index';
+
+                $router =& page()->router;
+                if ($class != $router->class || $method != $router->method)
+                {
+                    $router->redirect($notDone[0]);
+                }
+            }
         }
 
         private function uninstall()
