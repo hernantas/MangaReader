338a68d3de1a789ec5d4159f5627eecfe8dcd8b8
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-01 15:10:55 +0700

    New encryption method

--- system/library/Encryption.php
+++ system/library/Encryption.php
@@ -3,128 +3,163 @@
 
     /**
      * The Encryption class will provide encryption related function including hash or
-     * non-hash encryption
+     * non-hash encryption. Require MCrypt and MCRYPT_DEV_URANDOM
      *
      * @package Library
      */
     class Encryption
     {
-        /**
-         * Hash cost. More will make attack more harder but will cost resource more
-         *
-         * @var int
-         */
-        private $hashCost = 10;
+        private $key = '';
 
-        /**
-         * Algorithm that used for hash encryption
-         *
-         * @var int
-         */
-        private $hashAlgo = "$2a$%02d$";
+        private $hashAlgo = 'sha1';
 
-        /**
-         * Salt used as default. This shouldn't be used if possible. Use create_salt
-         * instead and store it in the safe place
-         *
-         * @var string
-         */
-        private $defaultSalt = '$2a$10$PkQ46MhLcgk4moG0hCm2aA==';
+        private $chiper = MCRYPT_RIJNDAEL_128;
+        private $mode = MCRYPT_MODE_CBC;
 
-        /**
-         * Create some random salt, used for encryption
-         *
-         * @return string New random salt
-         */
-        public function createSalt()
+        public function __construct()
         {
-            $salt = strtr(base64_encode(mcrypt_create_iv(16, MCRYPT_DEV_URANDOM)), '+', '.');
-            $salt = sprintf($this->hashAlgo, $this->hashCost) . $salt;
-            return $salt;
-        }
+            $cfg = page()->config->load('Encryption');
 
-        /**
-         * Do an encryption using hash algorithm
-         *
-         * @param  string $raw  String that want to be encrypted
-         * @param  string $salt Salt used to increase randomness
-         * @return string       Encryption result
-         */
-        public function hash($raw, $salt = '')
-        {
-            if ($salt === '') $salt = $this->createSalt();
-            return crypt($raw, $salt);
-        }
-
-        /**
-         * Compare hash encryption with raw data.
-         *
-         * @param  string $hash Hash that want to be compared
-         * @param  string $raw  Raw data that want to be compared
-         * @return bool         True if raw data is equal to the hash function or false otherwise.
-         */
-        public function hashEquals($hash, $raw)
-        {
-            return hash_equals($hash, crypt($raw, $hash));
-        }
-
-        /**
-         * Do 2-way encryption. It recommended to always generate salt for different
-         * encryption process since same salt will easily guessed.
-         *
-         * @param  string $raw      Raw data that want to be encrypted
-         * @param  string $password Password used to add defined salt
-         * @param  string $salt     Salt used in encryption. Will use $default_salt if empty
-         *
-         * @return string           Encrypted string
-         * @see Encryption::createSalt()
-         */
-        public function encrypt($raw, $salt='', $password='')
-        {
-            if ($salt === '')
+            if ($cfg === false || !isset($cfg['key']))
             {
-                $salt = $this->defaultSalt;
-            }
-            $key = hash('SHA256', $salt . $password, true);
-            srand();
-            // Create $iv and $iv_base64. Block size 128 bits (AES) and CBC mode.
-            $iv = mcrypt_create_iv(mcrypt_get_iv_size(MCRYPT_RIJNDAEL_128, MCRYPT_MODE_CBC), MCRYPT_RAND);
-            if (strlen($iv_base64 = rtrim(base64_encode($iv), '=')) != 22) return false;
+                $cfg['key'] = $this->createKey();
+                if (!isset($cfg['chiper'])) $cfg['chiper'] = MCRYPT_RIJNDAEL_128;
+                if (!isset($cfg['mode'])) $cfg['mode'] = MCRYPT_MODE_CBC;
 
-            $encrypted = base64_encode(mcrypt_encrypt(MCRYPT_RIJNDAEL_128, $key, $raw . md5($raw), MCRYPT_MODE_CBC, $iv));
-            return $iv_base64 . $encrypted;
+                page()->config->save('Encryption', $cfg);
+            }
+
+            $this->key = $cfg['key'];
+            $this->chiper = isset($cfg['chiper']) ? $cfg['chiper'] : MCRYPT_RIJNDAEL_128;
+            $this->mode = isset($cfg['mode']) ? $cfg['mode'] : MCRYPT_MODE_CBC;;
         }
 
         /**
-         * Do decryption proccess and return the result. The result will look like
-         * weird string if the salt used in an encryption is different.
+         * Create encryption key
          *
-         * @param  string $encrypted Encrypted data that need to be decrypted
-         * @param  string $password  Password used in encrypted data
-         * @param  string $salt      Salt used in encrypted data.
-         * @return string            Raw data if $salt and $password is the correct one.
+         * @param  int    $size Key size in byte
+         * @param  string $raw  If set to true, output will return raw output (binary)
+         *                      Otherwise will return base 64 string
+         * @return string       Return key that has been parsed to base 64 if $raw is false,
+         *                      return binary
          */
-        public function decrypt($encrypted, $password = '', $salt = '')
+        public function createKey($size=16, $raw=false)
         {
-            if ($salt === '')
+            $iv = mcrypt_create_iv($size, MCRYPT_DEV_URANDOM);
+            if ($raw)
             {
-                $salt = $this->defaultSalt;
+                return $iv;
             }
-            $key = hash('SHA256', $salt . $password, true);
-            // Retrieve $iv which is the first 22 characters plus ==, base64_decoded.
-            $iv = base64_decode(substr($encrypted, 0, 22) . '==');
-            // Remove $iv from $encrypted.
-            $encrypted = substr($encrypted, 22);
-            // Decrypt the data.  rtrim won't corrupt the data because the last 32 characters are the md5 hash; thus any \0 character has to be padding.
-            $decrypted = rtrim(mcrypt_decrypt(MCRYPT_RIJNDAEL_128, $key, base64_decode($encrypted), MCRYPT_MODE_CBC, $iv), "\0\4");
-            // Retrieve $hash which is the last 32 characters of $decrypted.
-            $hash = substr($decrypted, -32);
-            // Remove the last 32 characters from $decrypted.
-            $decrypted = substr($decrypted, 0, -32);
-            // Integrity check.  If this fails, either the data is corrupted, or the password/salt was incorrect.
-            if (md5($decrypted) != $hash) return false;
-            // Yay!
-            return $decrypted;
+            return base64_encode($iv);
+        }
+
+        /**
+         * Perform 2 way encryption
+         *
+         * @param  string $message Message that want to be encrypted
+         *
+         * @return string          Encrypted message
+         */
+        public function encrypt($message)
+        {
+            $key = base64_decode($this->key);
+            $iv = $this->createKey(mcrypt_get_iv_size($this->chiper, $this->mode), true);
+            $chipper = mcrypt_encrypt($this->chiper, $key, $message, $this->mode, $iv);
+            return base64_encode($iv.$chipper);
+        }
+
+        /**
+         * Perform decryption
+         *
+         * @param  string $chipper Encrypted message
+         *
+         * @return string          Decrypted message if success
+         */
+        public function decrypt($chipper)
+        {
+            $chipper = base64_decode($chipper);
+            $key = base64_decode($this->key);
+            $iv = substr($chipper, 0, mcrypt_get_iv_size($this->chiper, $this->mode));
+            $chipper = substr($chipper, mcrypt_get_iv_size($this->chiper, $this->mode));
+
+            $message = mcrypt_decrypt($this->chiper, $key, $chipper, $this->mode, $iv);
+
+            return $message;
+        }
+        /**
+         * Generate a hash string from input data. Don't use this method to encrypt
+         * password, instead use Encryption::hashPassword
+         *
+         * @param  string $message Message to be hashed
+         *
+         * @return string          Hashed message
+         */
+        public function hash($message)
+        {
+            return hash($this->hashAlgo, $message);
+        }
+
+        /**
+         * Get all list of registered hashing algorithm
+         *
+         * @return array List of hashing algorithm
+         */
+        public function hashAlgo()
+        {
+            return hash_algos();
+        }
+
+        /**
+         * Check if hash is equal to each other or not
+         *
+         * @param  string $str1 First hash to be compared
+         * @param  string $str2 Second hash
+         *
+         * @return bool         Return false if not same, true otherwise.
+         */
+        public function hashEquals($str1, $str2)
+        {
+            if(strlen($str1) != strlen($str2))
+            {
+                return false;
+            }
+            else
+            {
+                $res = $str1 ^ $str2;
+                $ret = 0;
+                for($i = strlen($res) - 1; $i >= 0; $i--)
+                {
+                    $ret |= ord($res[$i]);
+                }
+                return !$ret;
+            }
+        }
+
+        /**
+         * Generate a hash string from input data. This more targeted to be used
+         * for specific hashing like password or key. Algorithm to be used in hashing
+         * is Blowfish. Use Encryption::verifyPassword for verifying password/key
+         *
+         * @param  string $password  String that want to be encrypted
+         * @param  string $salt      Salt used to increase randomness
+         * @return string            Encryption result
+         */
+        public function hashPassword($password)
+        {
+            return password_hash($password, PASSWORD_BCRYPT);
+        }
+
+        /**
+         * Verify password matches a hash
+         *
+         * @param  string $password Password
+         * @param  string $hash     A hash create by Encryption::hashPassword
+         *
+         * @return bool             True if password match, false otherwise
+         */
+        public function verifyPassword($password, $hash)
+        {
+            return password_verify($password, $hash);
         }
     }
 ?>
