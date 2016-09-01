03e7a9f5bba36bc9ecdd6a01ec331f749c2c5f34
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-01 21:55:24 +0700

    Change navigation style

--- app/hook/Navigation.php
+++ app/hook/Navigation.php
@@ -5,46 +5,81 @@
     {
         public function view($data)
         {
-            return [
-                'nav' => [
-                    [
-                        [
-                            'url'=>'user/signin',
-                            'img'=>'user48.png',
-                            'text'=>'Sign In / Sign Up'
-                        ]
-                    ],
-                    [
-                        [
-                            'url'=>'',
-                            'img'=>'home48.png',
-                            'text'=>'Home'
-                        ],
-                        [
-                            'url'=>'manga/directory',
-                            'img'=>'directory48.png',
-                            'text'=>'Manga Directory'
-                        ],
-                        [
-                            'url'=>'manga/directory/hot',
-                            'img'=>'hot48.png',
-                            'text'=>'Popular Manga'
-                        ],
-                        [
-                            'url'=>'manga/directory/latest',
-                            'img'=>'news48.png',
-                            'text'=>'Latest Updated'
-                        ]
-                    ],
-                    [
-                        [
-                            'url'=>'uninstall',
-                            'img'=>'',
-                            'text'=>'Uninstall'
-                        ]
-                    ]
+            $nav = array();
+
+            if (page()->auth->isLoggedIn())
+            {
+                $nav[] = [[
+                    'url'=>'user/profile',
+                    'img'=>'catuser48.png',
+                    'text'=>'My Profiles'
+                ]];
+            }
+            else
+            {
+                $nav[] = [[
+                    'url'=>'user/signin',
+                    'img'=>'user48.png',
+                    'text'=>'Sign In / Sign Up'
+                ]];
+            }
+
+            $nav[] = [
+                [
+                    'url'=>'',
+                    'img'=>'home48.png',
+                    'text'=>'Home'
+                ],
+                [
+                    'url'=>'manga/directory',
+                    'img'=>'directory48.png',
+                    'text'=>'Manga Directory'
+                ],
+                [
+                    'url'=>'manga/directory/hot',
+                    'img'=>'hot48.png',
+                    'text'=>'Popular Manga'
+                ],
+                [
+                    'url'=>'manga/directory/latest',
+                    'img'=>'news48.png',
+                    'text'=>'Latest Updated'
                 ]
             ];
+
+            if (page()->auth->isLoggedIn())
+            {
+                if (page()->auth->getUserOption('privilege') == 'admin')
+                {
+                    $nav[] = [
+                        [
+                            'url'=>'scan',
+                            'img'=>'radar48.png',
+                            'text'=>'Scan Directory'
+                        ],
+                        [
+                            'url'=>'uninstall',
+                            'img'=>'destruct48.png',
+                            'text'=>'Uninstall'
+                        ]
+                    ];
+                }
+
+                $nav[] = [
+                    [
+                        'url'=>'user/history',
+                        'img'=>'paper48.png',
+                        'text'=>'History'
+                    ],
+                    [
+                        'url'=>'user/signout',
+                        'img'=>'leave48.png',
+                        'text'=>'Sign Out'
+                    ]
+                ];
+            }
+
+            return [ 'nav' => $nav ];
         }
     }
 
