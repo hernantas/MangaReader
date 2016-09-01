5e47ed5ccdfa1d80173839a085be7cb6aaacd6dc
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-01 20:05:25 +0700

    Add login proccess

--- app/page/SignIn.php
+++ app/page/SignIn.php
@@ -5,9 +5,33 @@
     {
         public function index()
         {
+            $this->auth->requireNoLogin();
+            
+            if ($this->input->hasPost())
+            {
+                $this->login();
+            }
+
             $this->load->storeView('SignIn');
 
             $this->load->layout('Fresh');
+        }
+
+        private function login()
+        {
+            $username = $this->input->post('username');
+            $password = $this->input->post('password');
+            $keep = $this->input->hasPost('keep');
+
+            if ($this->auth->createSession($username, $password, $keep))
+            {
+                $this->router->redirect('');
+            }
+            else
+            {
+                $this->message->error('Username or password is not valid, please'.
+                    ' check your username and password again.');
+            }
         }
     }
 
