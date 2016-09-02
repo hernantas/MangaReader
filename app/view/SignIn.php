f6956552122db77ece54e099f7252f3d5251f9b2
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-02 16:02:25 +0700

    Change new centering style

--- app/view/SignIn.php
+++ app/view/SignIn.php
@@ -1,22 +1,20 @@
-<div class="single">
-    <div class="panel warp">
-        <?php echo formOpen('user/signin'); ?>
-        <h1>Sign In</h1>
-        <div>
-            <?php echo inputText('username', 'Username'); ?>
-        </div>
-        <div>
-            <?php echo inputPassword('password', 'Password'); ?>
-        </div>
-        <div>
-            <?php echo inputCheckbox('keep', 'Remember Me'); ?>
-        </div>
-        <div>
-            <?php echo inputSubmit('Sign In'); ?>
-        </div>
-        <div>
-            Don't have an account yet? <a href="<?php echo baseUrl(); ?>user/signup">Create an Account</a>
-        </div>
-        <?php echo formClose(); ?>
+<div class="panel single">
+    <?php echo formOpen('user/signin'); ?>
+    <h1 class="warp">Sign In</h1>
+    <div class="warp center">
+        <?php echo inputText('username', 'Username'); ?>
     </div>
+    <div class="warp center">
+        <?php echo inputPassword('password', 'Password'); ?>
+    </div>
+    <div class="warp center">
+        <?php echo inputCheckbox('keep', 'Remember Me'); ?>
+    </div>
+    <div class="warp center">
+        <?php echo inputSubmit('Sign In'); ?>
+    </div>
+    <div class="warp center">
+        Don't have an account yet? <a href="<?php echo baseUrl(); ?>user/signup">Create an Account</a>
+    </div>
+    <?php echo formClose(); ?>
 </div>
--- app/view/SignUp.php
+++ app/view/SignUp.php
@@ -1,22 +1,20 @@
-<div class="single">
-    <div class="panel warp">
-        <?php echo formOpen('user/signup'); ?>
-        <h1>Create Account</h1>
-        <div>
-            <?php echo inputText('username', 'Username, 5-16 characters.'); ?>
-        </div>
-        <div>
-            <?php echo inputPassword('password', 'Password, 6-32 characters.'); ?>
-        </div>
-        <div>
-            <?php echo inputPassword('rpassword', 'Retype your password.'); ?>
-        </div>
-        <div>
-            <?php echo inputSubmit('Create my account'); ?>
-        </div>
-        <div>
-            Already have account? <a href="<?php echo baseUrl(); ?>user/signin">Sign In</a>
-        </div>
-        <?php echo formClose(); ?>
+<div class="panel single">
+    <?php echo formOpen('user/signup'); ?>
+    <h1>Create Account</h1>
+    <div class="warp center">
+        <?php echo inputText('username', 'Username, 5-16 characters.'); ?>
     </div>
+    <div class="warp center">
+        <?php echo inputPassword('password', 'Password, 6-32 characters.'); ?>
+    </div>
+    <div class="warp center">
+        <?php echo inputPassword('rpassword', 'Retype your password.'); ?>
+    </div>
+    <div class="warp center">
+        <?php echo inputSubmit('Create my account'); ?>
+    </div>
+    <div class="warp center">
+        Already have account? <a href="<?php echo baseUrl(); ?>user/signin">Sign In</a>
+    </div>
+    <?php echo formClose(); ?>
 </div>
