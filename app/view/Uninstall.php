027885687bba8ae140ed2ee6d677ab0b779ebd2f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-02 16:08:11 +0700

    Fix uninstall center style

--- app/view/Uninstall.php
+++ app/view/Uninstall.php
@@ -1,40 +1,37 @@
-<div  class="single">
-    <div class="panel warp">
-        <h1>Uninstall</h1>
-        <?php if (isset($alternative)): ?>
-            <div>
-                <b>Last Warning: </b>Are you sure you?
-            </div>
-            <div>
-                <a href="<?php echo baseUrl(); ?>">
-                    <?php echo inputButton('No'); ?>
-                </a>
-            </div>
-            <div>
-                <?php echo formOpen('admin/uninstall/warning'); ?>
-                    <?php echo inputHidden('uninstall', 'yes'); ?>
-                    <?php echo inputSubmit('Yes', 'alt'); ?>
-                <?php echo formClose(); ?>
-            </div>
+<div class="panel single">
+    <h1>Uninstall</h1>
+    <?php if (isset($alternative)): ?>
+        <div class="center warp">
+            <b>Last Warning: </b>Are you sure you?
+        </div>
+        <div class="center warp">
+            <a href="<?php echo baseUrl(); ?>">
+                <?php echo inputButton('No'); ?>
+            </a>
+        </div>
+        <div class="center warp">
+            <?php echo formOpen('admin/uninstall/warning'); ?>
+                <?php echo inputHidden('uninstall', 'yes'); ?>
+                <?php echo inputSubmit('Yes', 'alt'); ?>
+            <?php echo formClose(); ?>
+        </div>
 
-        <?php else: ?>
-            <div>
-                <b>Warning:</b> Be careful, when uninstallation progress begin, it can't be reverted.
-            </div>
-            <div>
-                Are you sure you want to uninstall this website application?
-            </div>
-            <div>
-                <a href="<?php echo baseUrl(); ?>">
-                    <?php echo inputButton('No'); ?>
-                </a>
-            </div>
-            <div>
-                <a href="<?php echo baseUrl(); ?>admin/uninstall/warning">
-                    <?php echo inputButton('Yes', 'alt'); ?>
-                </a>
-            </div>
-        <?php endif; ?>
-
-    </div>
+    <?php else: ?>
+        <div class="warp">
+            <b>Warning:</b> Be careful, when uninstallation progress begin, it can't be reverted.
+        </div>
+        <div class="warp">
+            Are you sure you want to uninstall this website application?
+        </div>
+        <div class="center warp">
+            <a href="<?php echo baseUrl(); ?>">
+                <?php echo inputButton('No'); ?>
+            </a>
+        </div>
+        <div class="center warp">
+            <a href="<?php echo baseUrl(); ?>admin/uninstall/warning">
+                <?php echo inputButton('Yes', 'alt'); ?>
+            </a>
+        </div>
+    <?php endif; ?>
 </div>
