49f9c9f536cafd2295a73f105b0715b96885a8a7
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-02 14:44:43 +0700

    Add input button and button alt class

--- system/helper/form.php
+++ system/helper/form.php
@@ -10,51 +10,60 @@
 
     if (!function_exists('inputText'))
     {
-        function inputText($name, $placeholder='', $value='', $readOnly=false)
+        function inputText($name, $placeholder='', $value='', $readOnly=false, $cssClass='')
         {
-            return input('text', $name, $value, $placeholder, $readOnly);
+            return input('text', $name, $value, $placeholder, $readOnly, $cssClass);
         }
     }
 
     if (!function_exists('inputSearch'))
     {
-        function inputSearch($name, $placeholder='', $value='')
+        function inputSearch($name, $placeholder='', $value='', $cssClass='')
         {
-            return input('search', $name, $value, $placeholder);
+            return input('search', $name, $value, $placeholder, false, $cssClass);
         }
     }
 
     if (!function_exists('inputPassword'))
     {
-        function inputPassword($name, $placeholder='')
+        function inputPassword($name, $placeholder='', $cssClass='')
         {
-            return input('password', $name, '', $placeholder);
+            return input('password', $name, '', $placeholder, false, $cssClass);
         }
     }
 
     if (!function_exists('inputCheckbox'))
     {
-        function inputCheckbox($name, $label, $value='')
+        function inputCheckbox($name, $label, $value='', $cssClass='')
         {
-            return '<label>' . input('checkbox', $name, $value===''?$name:$value) . $label . '</label>';
+            return '<label>' . input('checkbox', $name, $value===''?$name:$value, '', false, $cssClass) . $label . '</label>';
         }
     }
 
     if (!function_exists('inputSubmit'))
     {
-        function inputSubmit($value)
+        function inputSubmit($value, $cssClass='')
         {
-            return input('submit', '', $value, '');
+            return input('submit', '', $value, '', false, $cssClass);
+        }
+    }
+
+    if (!function_exists('inputButton'))
+    {
+        function inputButton($value, $cssClass='')
+        {
+            return input('button', '', $value, '', false, $cssClass);
         }
     }
 
     if (!function_exists('input'))
     {
-        function input($type='', $name, $value='', $placeholder='', $readOnly=false)
+        function input($type, $name, $value='', $placeholder='', $readOnly=false, $cssClass='')
         {
             return '<input type="'.$type.'" name="'.$name.'" value="' .
                 ($value === '' && $type != 'password' ? (page()->input->hasRequest($name) ? page()->input->request($name) : '') : $value) .
-                '" placeholder="'.$placeholder.'" '.($readOnly?'readonly="true"':'').' />';
+                '" placeholder="'.$placeholder.'" '.($readOnly?'readonly="true"':'') .
+                ($cssClass!==''?'class="'.$cssClass.'"':'').' />';
         }
     }
 
