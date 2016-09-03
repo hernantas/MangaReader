13ab9d59d9ae62732f3e9b248a82f0b5370f43b3
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-03 10:20:47 +0700

    Input now use array instead of parameter

--- system/helper/form.php
+++ system/helper/form.php
@@ -4,74 +4,125 @@
     {
         function inputHidden($name, $value)
         {
-            return input('hidden', $name, $value);
+            return input([
+                'type'=>'hidden',
+                'name'=>$name,
+                'value'=>$value
+            ]);
         }
     }
 
     if (!function_exists('inputText'))
     {
-        function inputText($name, $placeholder='', $value='', $readOnly=false, $cssClass='')
+        function inputText($name, $placeholder='', $value='', $readOnly=false, $css='')
         {
-            return input('text', $name, $value, $placeholder, $readOnly, $cssClass);
+            return input([
+                'type'=>'search',
+                'name'=>$name,
+                'placeholder'=>$placeholder,
+                'value'=>$value,
+                'readonly'=>$readOnly,
+                'css'=>$css
+            ]);
         }
     }
 
     if (!function_exists('inputSearch'))
     {
-        function inputSearch($name, $placeholder='', $value='', $cssClass='')
+        function inputSearch($name, $placeholder='', $css='')
         {
-            return input('search', $name, $value, $placeholder, false, $cssClass);
+            return input([
+                'type'=>'search',
+                'name'=>$name,
+                'placeholder'=>$placeholder,
+                'css'=>$css
+            ]);
         }
     }
 
     if (!function_exists('inputPassword'))
     {
-        function inputPassword($name, $placeholder='', $cssClass='')
+        function inputPassword($name, $placeholder='', $css='')
         {
-            return input('password', $name, '', $placeholder, false, $cssClass);
+            return input([
+                'type'=>'password',
+                'name'=>$name,
+                'placeholder'=>$placeholder,
+                'css'=>$css
+            ]);
         }
     }
 
     if (!function_exists('inputCheckbox'))
     {
-        function inputCheckbox($name, $label, $value='', $cssClass='')
+        function inputCheckbox($name, $label, $checked=false, $value='', $css='')
         {
-            return '<label>' . input('checkbox', $name, $value===''?$name:$value, '', false, $cssClass) . $label . '</label>';
+            return '<label>' . input([
+                'type'=>'checkbox',
+                'name'=>$name,
+                'value'=>$value,
+                'checked'=>$checked,
+                'css'=>$css
+            ]) . $label . '</label>';
         }
     }
 
     if (!function_exists('inputRadio'))
     {
-        function inputRadio($name, $value, $label, $default=false, $cssClass='')
+        function inputRadio($name, $value, $label, $checked=false, $css='')
         {
-            return '<label>' . input('radio', $name, $value, '', false, $cssClass) . $label . '</label>';
+            return '<label>' . input([
+                'type'=>'radio',
+                'name'=>$name,
+                'value'=>$value,
+                'checked'=>$checked,
+                'css'=>$css
+            ]) . $label . '</label>';
         }
     }
 
     if (!function_exists('inputSubmit'))
     {
-        function inputSubmit($value, $cssClass='')
+        function inputSubmit($value, $css='')
         {
-            return input('submit', '', $value, '', false, $cssClass);
+            return input([
+                'type'=>'submit',
+                'value'=>$value,
+                'css'=>$css
+            ]);
         }
     }
 
     if (!function_exists('inputButton'))
     {
-        function inputButton($value, $cssClass='')
+        function inputButton($value, $css='')
         {
-            return input('button', '', $value, '', false, $cssClass);
+            return input([
+                'type'=>'button',
+                'value'=>$value,
+                'css'=>$css
+            ]);
         }
     }
 
     if (!function_exists('input'))
     {
-        function input($type, $name, $value='', $placeholder='', $readOnly=false, $cssClass='')
+        function input($option=array())
         {
-            return '<input type="'.$type.'" name="'.$name.'" value="' .
-                ($value === '' && $type != 'password' ? (page()->input->hasRequest($name) ? page()->input->request($name) : '') : $value) .
-                '" placeholder="'.$placeholder.'" '.($readOnly?'readonly="true"':'') .
-                ($cssClass!==''?'class="'.$cssClass.'"':'').' />';
+            if (!isset($option['type']))
+            {
+                logError('No type input', 'Helper Form');
+                return '';
+            }
+
+            return '<input type="' . $option['type'] . '"' .
+                (isset($option['name']) ? ' name="'.$option['name'].'"' : '') .
+                (isset($option['value']) && $option['value'] !== '' ? ' value="'.$option['value'].'"' : '') .
+                (isset($option['placeholder']) ? ' placeholder="'.$option['placeholder'].'"' : '') .
+                (isset($option['checked']) && $option['checked'] ? ' checked="checked"' : '') .
+                (isset($option['readonly']) && $option['readonly'] ? ' readonly="readonly"' : '') .
+                (isset($option['css']) && $option['css'] !== '' ? ' class="'.$option['css'].'"' : '') .
+                ' />';
         }
     }
 
