ca8cc93e99e615e74e21dde2e3830377f8477b89
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-03 10:21:59 +0700

    Fix centering at install page

--- app/view/InstallDatabase.php
+++ app/view/InstallDatabase.php
@@ -2,35 +2,30 @@
     <div class="warp">
         <?php echo formOpen('install/database'); ?>
         <h1>Set Up Database</h1>
-        <div>
+        <div class="warp">
             Now, you must input your database configuration.
         </div>
-        <div>
+        <div class="warp center">
             <div><b>Database Host</b></div>
             <?php echo inputText('host', '', 'localhost'); ?>
             <div class="desc small">Database host, example: localhost</div>
         </div>
-        <div>
+        <div class="warp center">
             <div><b>Database Username</b></div>
             <?php echo inputText('username', '', 'root'); ?>
             <div class="desc small">Database username, example: root</div>
         </div>
-        <div>
+        <div class="warp center">
             <div><b>Database Password</b></div>
             <?php echo inputPassword('password', '', ''); ?>
             <div class="desc small">Your database password</div>
         </div>
-        <div>
+        <div class="warp center">
             <div><b>Database Name</b></div>
             <?php echo inputText('name', '', 'mediadb'); ?>
             <div class="desc small">Database name, example: mediadb</div>
         </div>
-        <div>
-            <span></span>
-
-            <span></span>
-        </div>
-        <div>
+        <div class="warp center">
             <?php echo inputSubmit('Continue'); ?>
         </div>
         <?php echo formClose(); ?>
--- app/view/InstallWelcome.php
+++ app/view/InstallWelcome.php
@@ -1,4 +1,4 @@
-<div class="single panel">
+<div class="panel single">
     <div class="warp">
         <?php echo formOpen('install'); ?>
         <h1>Welcome</h1>
@@ -8,7 +8,7 @@
             First, read the agreement bellow and accept the agreement if you want to use
             this website.
         </div>
-        <div>
+        <div class="center">
             <pre>
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
 EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
@@ -19,11 +19,11 @@ CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
 WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
             </pre>
         </div>
-        <div>
+        <div class="center warp">
             <?php echo inputHidden('license', 'true') ?>
             <?php echo inputCheckbox('agree', 'I have read and agree.', 'agree'); ?>
         </div>
-        <div>
+        <div class="center">
             <?php echo inputSubmit('Continue'); ?>
         </div>
         <?php echo formClose(); ?>
