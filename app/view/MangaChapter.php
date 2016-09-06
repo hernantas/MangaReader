9b59114ed983c6f6bc9f16a64e843e638124c9c1
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-06 14:01:31 +0700

    Change manga chapter design

--- app/view/MangaChapter.php
+++ app/view/MangaChapter.php
@@ -1,14 +1,30 @@
 <div class="panel single">
-    <div class="warp">
+    <div class="hwarp">
         <h1><?php echo $manga->name; ?></h1>
     </div>
+    <div class="warp center">
+        <?php $this->view('MangaTag', ['manga'=>$manga]); ?>
+    </div>
     <div class="warp">
+        <div>Rank: <?php echo $manga->rankings; ?></div>
+        <div>Views: <?php echo $manga->views; ?> times</div>
+        <div class="desc">
+            <?php echo page()->date->relative($manga->update_at); ?>
+        </div>
+    </div>
+</div>
+
+<div class="panel single">
+    <div class="hwarp">
+        <h4>Chapters:</h4>
+    </div>
+    <div>
         <?php foreach($order as $ord): ?>
             <?php $chapter = $chapters[$ord]; ?>
             <div class="list">
                 <a href="<?php echo baseUrl().'manga/'.$manga->friendly_name.'/chapter/'.$chapter->friendly_name; ?>"
                     class="clearfix">
-                    <div class="left">
+                    <div class="left hwarp">
                         <?php echo $ord; ?>
                     </div>
                     <div class="right desc">
