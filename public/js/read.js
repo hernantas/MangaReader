28b8a89967c95770e32d5c97e5beda5c3a7a16fc
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-06 14:01:42 +0700

    Add jump javascript at read page

--- public/js/read.js
+++ public/js/read.js
@@ -3,6 +3,9 @@ $(document).ready(function() {
     $(document).keyup(function(e) {
         if (e.keyCode === 37) window.location.href = prevPage;
         if (e.keyCode === 39) window.location.href = nextPage;
+    });
 
+    $(".jump").on('change', function() {
+        window.location.href = baseUrl + this.value;
     });
 });
