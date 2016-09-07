5a70407c4e474fdd613a03b5242153860aa12608
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-07 07:59:41 +0700

    Change how fixedwidth calculated

--- public/js/layout.js
+++ public/js/layout.js
@@ -1,13 +1,18 @@
 
 var fixedWidth = 220;
+var minWidth = 660;
 var fixedPadding = 100;
 
 function layoutWidth()
 {
+    var wWidth = $(window).width();
+    var bodyPadding = Math.max(Math.floor(wWidth / fixedWidth)-3, 1) * 100;
     if ($(".container").length)
     {
-        var width = $(window).width()-fixedPadding;
-        $(".container").width(Math.floor(width / fixedWidth) * fixedWidth);
+        var width = wWidth - bodyPadding;
+        var newWidth = Math.floor(width / fixedWidth) * fixedWidth;
+        if (newWidth < minWidth) newWidth = minWidth;
+        $(".container").width(newWidth);
     }
 }
 
