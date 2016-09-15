f6448d360502678571d393edecdfc07aa08bce80
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-15 20:57:01 +0700

    Rename parameter $ts to $timestamp

--- system/library/Date.php
+++ system/library/Date.php
@@ -3,14 +3,14 @@
 
     class Date
     {
-        public function relative($ts)
+        public function relative($timestamp)
         {
-            if(!ctype_digit($ts))
+            if(!ctype_digit($timestamp))
             {
-                $ts = strtotime($ts);
+                $timestamp = strtotime($timestamp);
             }
 
-            $diff = time() - $ts;
+            $diff = time() - $timestamp;
             if($diff == 0)
             {
                 return 'now';
@@ -30,7 +30,7 @@
                 if($day_diff < 7) return $day_diff . ' days ago';
                 if($day_diff < 31) return ceil($day_diff / 7) . ' weeks ago';
                 if($day_diff < 60) return 'last month';
-                return date('F Y', $ts);
+                return date('F Y', $timestamp);
             }
             else
             {
@@ -44,11 +44,11 @@
                     if($diff < 86400) return 'in ' . floor($diff / 3600) . ' hours';
                 }
                 if($day_diff == 1) return 'Tomorrow';
-                if($day_diff < 4) return date('l', $ts);
+                if($day_diff < 4) return date('l', $timestamp);
                 if($day_diff < 7 + (7 - date('w'))) return 'next week';
                 if(ceil($day_diff / 7) < 4) return 'in ' . ceil($day_diff / 7) . ' weeks';
-                if(date('n', $ts) == date('n') + 1) return 'next month';
-                return date('F Y', $ts);
+                if(date('n', $timestamp) == date('n') + 1) return 'next month';
+                return date('F Y', $timestamp);
             }
         }
     }
