ee0eeefd465b7725cb9b80a1057ee41e343df08f
Zulfikar Raditya Hernanta <hernantas@gmail.com>
2016-09-23 12:36:45 +0700

    Fix how image library handling image type (fake extension such as png but extension is jpg)

--- system/library/Image.php
+++ system/library/Image.php
@@ -7,95 +7,134 @@
 
         public function getContent($path)
         {
-            $type = pathinfo ($path, PATHINFO_EXTENSION);
+            $info = $this->getImageData($path);
             $name = md5($path);
 
-            if (file_exists(BASE_PATH.$this->savePath.$name.'.'.$type))
+            if (file_exists(BASE_PATH.$this->savePath.$name.'.'.$info['typeString']))
             {
-                return baseUrl().$this->savePath.$name.'.'.$type;
+                return baseUrl().$this->savePath.$name.'.'.$info['typeString'];
             }
 
-            $image = $this->createNewImage($type, $path);
-            $this->output($type, $image, BASE_PATH.$this->savePath.$name.'.'.$type);
+            $image = $this->createNewImage($info['type'], $path);
+            $this->output($info['type'], $image, BASE_PATH.$this->savePath.$name.'.'.$info['typeString']);
             imagedestroy($image);
-            return baseUrl().$this->savePath.$name.'.'.$type;
+            return baseUrl().$this->savePath.$name.'.'.$info['typeString'];
         }
 
         public function getContent64($path)
         {
-            $type = pathinfo ($path, PATHINFO_EXTENSION);
-            list($width, $height) = getimagesize($path);
-            $image = $this->createNewImage($type, $path);
+            $info = $this->getImageData($path);
+            $image = $this->createNewImage($info['type'], $path);
             return [
-                'img'=>$this->output64($type, $image),
-                'width'=>$width,
-                'height'=>$height
+                'img'=>$this->output64($info['type'], $image),
+                'width'=>$info['width'],
+                'height'=>$info['height']
             ];
         }
 
         public function getContentCrop($path, $width, $height)
         {
-            $type = pathinfo ($path, PATHINFO_EXTENSION);
+            $info = $this->getImageData($path);
             $name = md5($path . $width . $height . 'crop');
 
-            if (file_exists(BASE_PATH.$this->savePath.$name.'.'.$type))
+            if (file_exists(BASE_PATH.$this->savePath.$name.'.'.$info['typeString']))
             {
-                return baseUrl().$this->savePath.$name.'.'.$type;
+                return baseUrl().$this->savePath.$name.'.'.$info['typeString'];
             }
 
             // Generate New Image
-            list($oldWidth, $oldHeight) = getimagesize($path);
+            $oldWidth = $info['width'];
+            $oldHeight = $info['height'];
             $ratio = ($oldWidth < $oldHeight) ?
                 ($width / $oldWidth) : ($height / $oldHeight);
 
             $newImage = imagecreatetruecolor($width, $height);
-            $image = $this->createNewImage($type, $path);
+            $image = $this->createNewImage($info['type'], $path);
 
             imagecopyresampled($newImage, $image, 0, 0, 0, 0,
                 $oldWidth * $ratio, $oldHeight * $ratio, $oldWidth, $oldHeight);
             // imagecopy($cropedImage, $resizedImage, 0, 0, $width, $height, $oldWidth * $ratio, $oldHeight * $ratio);
 
-            $this->output($type, $newImage, BASE_PATH.$this->savePath.$name.'.'.$type);
+            $this->output($info['type'], $newImage, BASE_PATH.$this->savePath.$name.'.'.$info['typeString']);
             imagedestroy($newImage);
             imagedestroy($image);
-            return baseUrl().$this->savePath.$name.'.'.$type;
+            return baseUrl().$this->savePath.$name.'.'.$info['typeString'];
+        }
+
+        private function getImageData($path)
+        {
+            list($width, $height, $type) = getimagesize($path);
+            $typestring = '';
+            if ($type == IMAGETYPE_JPEG)
+            {
+                $typeString = 'jpeg';
+            }
+        	elseif ($type == IMAGETYPE_PNG)
+            {
+                $typeString = 'png';
+            }
+        	elseif ($type == IMAGETYPE_GIF)
+            {
+                $typeString = 'gif';
+            }
+            return [
+                'width'=>$width,
+                'height'=>$height,
+                'type'=>$type,
+                'typeString'=>$typeString
+            ];
         }
 
         private function createNewImage($type, $path)
         {
-            if ($type==='jpg' || $type==='jpeg')
+            $image = null;
+            switch ($type)
             {
-                return imagecreatefromjpeg($path);
+                case IMAGETYPE_JPEG:
+                    $image = imagecreatefromjpeg($path);
+                    break;
+                case IMAGETYPE_PNG:
+                    $image = imagecreatefrompng($path);
+                    imagealphablending($image, true);
+                	imagesavealpha($image, true);
+                    break;
+                case IMAGETYPE_GIF:
+                    $image = imagecreatefromgif($path);
+                    break;
             }
-            else if ($type==='png')
-            {
-                return imagecreatefrompng($path);
-            }
+            return $image;
         }
 
         private function output($type, $img, $output)
         {
-            if ($type==='jpg' || $type==='jpeg')
+            switch ($type)
             {
-                imagejpeg($img, $output);
-            }
-            else if ($type==='png')
-            {
-                imagepng($img, $output);
+                case IMAGETYPE_JPEG:
+                    imagejpeg($img, $output);
+                    break;
+                case IMAGETYPE_PNG:
+                    imagepng($img, $output);
+                    break;
+                case IMAGETYPE_GIF:
+                    imagegif($img, $output);
+                    break;
             }
         }
 
         private function output64($type, $img)
         {
             ob_start();
-            if ($type==='jpg' || $type==='jpeg')
+            switch ($type)
             {
-                $type = 'jpeg';
-                imagejpeg($img);
-            }
-            else if ($type==='png')
-            {
-                imagepng($img);
+                case IMAGETYPE_JPEG:
+                    imagejpeg($img);
+                    break;
+                case IMAGETYPE_PNG:
+                    imagepng($img);
+                    break;
+                case IMAGETYPE_GIF:
+                    imagegif($img);
+                    break;
             }
             $output = ob_get_contents();
             ob_end_clean();
